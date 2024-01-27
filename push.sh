45fb4f6f9f23eb778442cd9a1b3ae98cacb78069
bunjee <bunjee@omega.gg>
2024-01-27 18:07:03 +0100

    Add configure.sh and push.sh (2)

--- push.sh
+++ push.sh
@@ -8,7 +8,10 @@ set -e
 # NOTE: Update this path with your own asset folder.
 assets="/c/users/bunjee/OneDrive/assets/indy"
 
+deploy="$PWD/deploy"
 dist="$PWD/dist"
+
+trailer="$dist/trailer"
 
 indy="$dist/character/indy"
 
@@ -19,11 +22,15 @@ attic="$dist/room/attic"
 # Syntax
 #--------------------------------------------------------------------------------------------------
 
-if [ $# != 1 ] \
-   || \
-   [ $1 != "character/indy" -a $1 != "room/intro" -a $1 != "room/attic" ]; then
+if [ $# != 1 ] || [ $1 != "deploy"         -a \
+                    $1 != "trailer"        -a \
+                    $1 != "character/indy" -a \
+                    $1 != "room/intro"     -a \
+                    $1 != "room/attic" ]; then
 
-    echo "Usage: push <character/indy>"
+    echo "Usage: push <deploy>"
+    echo "            <trailer>"
+    echo "            <character/indy>"
     echo "            <room/intro>"
     echo "            <room/attic>"
 
@@ -33,6 +40,32 @@ fi
 read -p "Run push for $1 ? (yes/no) " REPLY
 
 if [ "$REPLY" != "yes" ]; then exit 1; fi
+
+#--------------------------------------------------------------------------------------------------
+# Deploy
+#--------------------------------------------------------------------------------------------------
+
+if [ $1 = "deploy" ]; then
+
+    path="$assets/deploy"
+    mkdir -p "$path"
+
+    cp "$deploy"/attic.mp4    "$path"
+    cp "$deploy"/trailer.mp4  "$path"
+    cp "$deploy"/trailer2.mp4 "$path"
+fi
+
+#--------------------------------------------------------------------------------------------------
+# Trailer
+#--------------------------------------------------------------------------------------------------
+
+if [ $1 = "trailer" ]; then
+
+    path="$assets/trailer/content"
+    mkdir -p "$path"
+
+    cp "$trailer"/content/indy.mp4 "$path"
+fi
 
 #--------------------------------------------------------------------------------------------------
 # Character indy
