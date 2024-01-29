df162f369f4da681d3bca165193501d696e77175
bunjee <bunjee@omega.gg>
2024-01-29 11:17:55 +0100

    Add configure.sh and push.sh (6)

--- configure.sh
+++ configure.sh
@@ -63,3 +63,125 @@ if [ $1 = "trailer" ]; then
 
     cp "$trailer"/content/indy.mp4 "$path"
 fi
+
+#--------------------------------------------------------------------------------------------------
+# Character indy
+#--------------------------------------------------------------------------------------------------
+
+if [ $1 = "character/indy" ]; then
+
+    path="$PWD/dist/character/indy/reference/body"
+
+    cp "$indy"/reference/body/indy.jpg "$path"
+
+    path="$PWD/dist/character/indy/reference/neutral"
+
+    cp "$indy"/reference/neutral/indy.jpg   "$path"
+    cp "$indy"/reference/neutral/indy2.webp "$path"
+    cp "$indy"/reference/neutral/indy3.png  "$path"
+
+    path="$PWD/dist/character/indy/reference/neutral/base"
+
+    cp "$indy"/reference/neutral/base/indy3.jpg "$path"
+
+    path="$PWD/dist/character/indy/reference/surprised"
+
+    cp "$indy"/reference/surprised/indy.png  "$path"
+    cp "$indy"/reference/surprised/indy2.png "$path"
+
+    path="$PWD/dist/character/indy/reference/surprised/base"
+
+    cp "$indy"/reference/surprised/base/indy.jpg  "$path"
+    cp "$indy"/reference/surprised/base/indy2.jpg "$path"
+
+    path="$PWD/dist/character/indy/data/voice"
+
+    cp "$indy"/data/voice/neutral.webm "$path"
+fi
+
+#--------------------------------------------------------------------------------------------------
+# Room intro
+#--------------------------------------------------------------------------------------------------
+
+if [ $1 = "room/intro" ]; then
+
+    path="$PWD/dist/room/intro/content"
+
+    cp "$intro"/content/intro.mp4     "$path"
+    cp "$intro"/content/intro2.mp4    "$path"
+    cp "$intro"/content/intro3.mp4    "$path"
+    cp "$intro"/content/lucasarts.jpg "$path"
+    cp "$intro"/content/title.png     "$path"
+fi
+
+#--------------------------------------------------------------------------------------------------
+# Room attic
+#--------------------------------------------------------------------------------------------------
+
+if [ $1 = "room/attic" ]; then
+
+    path="$PWD/dist/room/attic/reference"
+
+    cp "$attic"/reference/attic.jpg   "$path"
+    cp "$attic"/reference/splash.psd  "$path"
+    cp "$attic"/reference/splash.png  "$path"
+    cp "$attic"/reference/splash2.psd "$path"
+    cp "$attic"/reference/splash2.png "$path"
+    cp "$attic"/reference/splash3.psd "$path"
+    cp "$attic"/reference/splash3.png "$path"
+
+    path="$PWD/dist/room/attic/data"
+
+    cp "$attic"/data/attic.psd  "$path"
+    cp "$attic"/data/attic.png  "$path"
+    cp "$attic"/data/attic2.psd "$path"
+
+    cp "$attic"/data/indy.psd    "$path"
+    cp "$attic"/data/indy.png    "$path"
+    cp "$attic"/data/indy2.psd   "$path"
+    cp "$attic"/data/indy2.png   "$path"
+    cp "$attic"/data/indy3.psd   "$path"
+    cp "$attic"/data/indy3.png   "$path"
+    cp "$attic"/data/indy3.psd   "$path"
+    cp "$attic"/data/indy4.png   "$path"
+    cp "$attic"/data/indy5.png   "$path"
+    cp "$attic"/data/splash.psd  "$path"
+    cp "$attic"/data/splash2.psd "$path"
+    cp "$attic"/data/splash3.psd "$path"
+
+    path="$PWD/dist/room/attic/data/upscale"
+
+    cp "$attic"/data/upscale/attic.jpeg "$path"
+    cp "$attic"/data/upscale/indy.jpeg  "$path"
+    cp "$attic"/data/upscale/indy2.jpeg "$path"
+
+    path="$PWD/dist/room/attic/content"
+
+    cp "$attic"/content/attic.mp4   "$path"
+    cp "$attic"/content/attic.png   "$path"
+    cp "$attic"/content/attic2.mp4  "$path"
+    cp "$attic"/content/attic2.png  "$path"
+    cp "$attic"/content/attic3.mp4  "$path"
+    cp "$attic"/content/attic4.mp4  "$path"
+    cp "$attic"/content/indy2.mp4   "$path"
+    cp "$attic"/content/indy3.png   "$path"
+    cp "$attic"/content/splash.png  "$path"
+    cp "$attic"/content/splash.mp4  "$path"
+    cp "$attic"/content/splash2.png "$path"
+    cp "$attic"/content/splash2.mp4 "$path"
+    cp "$attic"/content/splash3.png "$path"
+
+    path="$PWD/dist/room/attic/content/audio"
+
+    cp "$attic"/content/audio/splash.mp3 "$path"
+
+    path="$PWD/dist/room/attic/content/music"
+
+    cp "$attic"/content/music/attic.webm "$path"
+
+    path="$PWD/dist/room/attic/content/voice"
+
+    cp "$attic"/content/voice/attic.mp3  "$path"
+    cp "$attic"/content/voice/attic2.mp3 "$path"
+    cp "$attic"/content/voice/attic3.mp3 "$path"
+fi
