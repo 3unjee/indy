447b5302989414e404ebdcab18d16f61e404a16d
Benjamin Arnaud <barnaud@slb.com>
2024-08-23 18:40:06 +0200

    room: Create attic2 (19)

--- dist/room/attic/content/upscale.sh
+++ dist/room/attic/content/upscale.sh
@@ -33,20 +33,20 @@ run()
 
 cd "$ffmpeg"
 
-#run "attic.mp4"     iris-3
-#run "attic2.mp4"    iris-3
-#run "attic5.mp4"    iris-3
-#run "attic6.mp4"    iris-3
-#run "attic7.mp4"    iris-3
-#run "attic8.mp4"    iris-3
-#run "attic11.mp4"   iris-3
-#run "attic12.mp4"   iris-3
-#run "attic13.mp4"   rhea-1 crop
-#run "attic14.mp4"   rhea-1 crop
+run "attic.mp4"     iris-3
+run "attic2.mp4"    iris-3
+run "attic5.mp4"    iris-3
+run "attic6.mp4"    iris-3
+run "attic7.mp4"    iris-3
+run "attic8.mp4"    iris-3
+run "attic11.mp4"   iris-3
+run "attic12.mp4"   iris-3
+run "attic13.mp4"   rhea-1 crop
+run "attic14.mp4"   rhea-1 crop
 run "indy3.mp4"     iris-3
-#run "indy4.mp4"     iris-3
-#run "splash3.mp4"   iris-3
-#run "desk.mp4"      iris-3
-#run "cross6.mp4"    iris-3
-#run "cross7.mp4"    iris-3
-#run "gargoyle6.mp4" iris-3
+run "indy4.mp4"     iris-3
+run "splash3.mp4"   iris-3
+run "desk.mp4"      iris-3
+run "cross6.mp4"    iris-3
+run "cross7.mp4"    iris-3
+run "gargoyle6.mp4" iris-3
