f3e7d879d180ec3c79fcb49463b223c89e0a0dd1
Benjamin Arnaud <bunjee@omega.gg>
2024-09-13 15:30:41 +0200

    dist: Create movie (6)

--- dist/room/attic/content/upscale.sh
+++ dist/room/attic/content/upscale.sh
@@ -33,26 +33,26 @@ run()
 
 cd "$ffmpeg"
 
-run "attic.mp4"     iris-3
-run "attic2.mp4"    iris-3
-run "attic5.mp4"    iris-3
-run "attic6.mp4"    iris-3
-run "attic7.mp4"    iris-3
-run "attic8.mp4"    iris-3
-run "attic11.mp4"   iris-3
-run "attic12.mp4"   iris-3
-run "attic13.mp4"   rhea-1 crop
-run "attic14.mp4"   rhea-1 crop
-run "attic16.mp4"   iris-3
-run "attic17.mp4"   iris-3
+#run "attic.mp4"     iris-3
+#run "attic2.mp4"    iris-3
+#run "attic5.mp4"    iris-3
+#run "attic6.mp4"    iris-3
+#run "attic7.mp4"    iris-3
+#run "attic8.mp4"    iris-3
+#run "attic11.mp4"   iris-3
+#run "attic12.mp4"   iris-3
+#run "attic13.mp4"   rhea-1 crop
+#run "attic14.mp4"   rhea-1 crop
+#run "attic16.mp4"   iris-3
+#run "attic17.mp4"   iris-3
 run "attic18.mp4"   iris-3
-run "attic19.mp4"   iris-3
-run "attic20.mp4"   iris-3
-run "attic21.mp4"   iris-3
-run "indy3.mp4"     iris-3
-run "indy4.mp4"     iris-3
-run "splash3.mp4"   iris-3
-run "desk.mp4"      iris-3
-run "cross6.mp4"    iris-3
-run "cross7.mp4"    iris-3
-run "gargoyle6.mp4" iris-3
+#run "attic19.mp4"   iris-3
+#run "attic20.mp4"   iris-3
+#run "attic21.mp4"   iris-3
+#run "indy3.mp4"     iris-3
+#run "indy4.mp4"     iris-3
+#run "splash3.mp4"   iris-3
+#run "desk.mp4"      iris-3
+#run "cross6.mp4"    iris-3
+#run "cross7.mp4"    iris-3
+#run "gargoyle6.mp4" iris-3
