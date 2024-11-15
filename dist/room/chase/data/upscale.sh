69be8095102847760127bf8259af1ce3bc94c980
Benjamin Arnaud <bunjee@omega.gg>
2024-11-15 10:10:35 +0100

    room: Create chase (45)

--- dist/room/chase/data/upscale.sh
+++ dist/room/chase/data/upscale.sh
@@ -7,9 +7,13 @@ set -e
 
 Magnific="$PWD/../../../../../Sky/tools/Magnific"
 
+imageConverter="$PWD/../../../../../Sky/deploy/imageConverter"
+
 input="$PWD/upscale/base"
 
 output="$PWD/upscale"
+
+screens="$PWD/../../../screens/chase"
 
 #--------------------------------------------------------------------------------------------------
 # Functions
@@ -23,6 +27,13 @@ run()
     else
         sh upscale.sh "$input/$1.png" "$output/$1.png" $2 films_n_photography
     fi
+
+    "$imageConverter" "$output/$1.png" "$output/jpg/$1.jpg" jpg 80 2560 1435
+}
+
+copy()
+{
+    cp -v "$output/jpg/$1.jpg" "$screens"
 }
 
 #--------------------------------------------------------------------------------------------------
@@ -38,3 +49,14 @@ run "interior2" 2x -2
 run "interior3" 2x -2
 run "interior4" 2x -2
 run "interior5" 2x -2
+
+#--------------------------------------------------------------------------------------------------
+# Screens
+#--------------------------------------------------------------------------------------------------
+
+copy "college"
+copy "college2"
+copy "interior2"
+copy "interior3"
+copy "interior4"
+copy "interior5"
