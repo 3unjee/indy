6239501c1082f84b5230df0bbc07a268eb506e54
Benjamin Arnaud <bunjee@omega.gg>
2025-06-13 10:01:38 +0200

    room: Update attic (5)

--- dist/room/attic/content/copy.sh
+++ dist/room/attic/content/copy.sh
@@ -15,12 +15,30 @@ temp="$upscale/temp"
 
 run()
 {
-    cp "$1" "$upscale"
+    cp "$1" "$upscale/$1"
 }
 
 exists()
 {
     ls "$1" 1> /dev/null 2>&1
+}
+
+move()
+{
+    if exists "$1"/*.mp4; then
+
+        mv "$1"/*.mp4 "$2"
+    fi
+}
+
+restore()
+{
+    if exists "$1"/*.mp4; then
+
+        rm -f "$2"/*.mp4
+
+        mv "$1"/*.mp4 "$2"
+    fi
 }
 
 #--------------------------------------------------------------------------------------------------
@@ -29,17 +47,11 @@ exists()
 
 if [ "$1" = "restore" ]; then
 
-    if exists "$temp"/*.mp4; then
-
-        rm -f "$upscale"/*.mp4
-
-        mv "$temp"/*.mp4 "$upscale"
-    fi
+    restore "$temp"      "$upscale"
+    restore "$temp/wide" "$upscale/wide"
 else
-    if exists "$upscale"/*.mp4; then
-
-        mv "$upscale"/*.mp4 "$temp"
-    fi
+    move "$upscale"      "$temp"
+    move "$upscale/wide" "$temp/wide"
 
     #----------------------------------------------------------------------------------------------
     # NOTE: copied from upscale.sh
@@ -75,4 +87,10 @@ else
     run "dialog-sync.mp4"  iris-3 letterbox
     run "dialog2-sync.mp4" iris-3 letterbox
     run "dialog3-sync.mp4" iris-3 letterbox
+
+    run "wide/splash3.mp4"   iris-3 default 5110
+    run "wide/attic22.mp4"   iris-3 default 5110
+    run "wide/indy4.mp4"     iris-3 default 5110
+    run "wide/dialog3-1.mp4" iris-3 default 5110
+    run "wide/dialog3-2.mp4" iris-3 default 5110
 fi
--- dist/room/attic/content/upscale.sh
+++ dist/room/attic/content/upscale.sh
@@ -33,39 +33,39 @@ run()
 
 cd "$ffmpeg"
 
-#run "attic.mp4"     iris-3
-#run "attic2.mp4"    iris-3
-#run "attic5.mp4"    iris-3
-#run "attic6.mp4"    iris-3
-#run "attic7.mp4"    iris-3
-#run "attic8.mp4"    iris-3
-#run "attic11.mp4"   iris-3
-#run "attic12.mp4"   iris-3
-#run "attic13.mp4"   rhea-1 crop
-#run "attic14.mp4"   rhea-1 crop
-#run "attic18.mp4"   iris-3
-#run "attic19.mp4"   iris-3
-#run "attic20.mp4"   iris-3
-#run "attic22.mp4"   iris-3
-#run "attic24.mp4"   iris-3
-#run "attic25.mp4"   iris-3
+run "attic.mp4"     iris-3
+run "attic2.mp4"    iris-3
+run "attic5.mp4"    iris-3
+run "attic6.mp4"    iris-3
+run "attic7.mp4"    iris-3
+run "attic8.mp4"    iris-3
+run "attic11.mp4"   iris-3
+run "attic12.mp4"   iris-3
+run "attic13.mp4"   rhea-1 crop
+run "attic14.mp4"   rhea-1 crop
+run "attic18.mp4"   iris-3
+run "attic19.mp4"   iris-3
+run "attic20.mp4"   iris-3
+run "attic22.mp4"   iris-3
+run "attic24.mp4"   iris-3
+run "attic25.mp4"   iris-3
 
-#run "indy4.mp4"     iris-3
-#run "splash3.mp4"   iris-3
-#run "desk.mp4"      iris-3
-#run "cross6.mp4"    iris-3
-#run "cross7.mp4"    iris-3
-#run "gargoyle6.mp4" iris-3
+run "indy4.mp4"     iris-3
+run "splash3.mp4"   iris-3
+run "desk.mp4"      iris-3
+run "cross6.mp4"    iris-3
+run "cross7.mp4"    iris-3
+run "gargoyle6.mp4" iris-3
 
-#run "dialog.mp4"  iris-3
-#run "dialog2.mp4" iris-3
-#run "dialog3.mp4" iris-3
+run "dialog.mp4"  iris-3
+run "dialog2.mp4" iris-3
+run "dialog3.mp4" iris-3
 
-#run "dialog-sync.mp4"  iris-3 letterbox
-#run "dialog2-sync.mp4" iris-3 letterbox
-#run "dialog3-sync.mp4" iris-3 letterbox
+run "dialog-sync.mp4"  iris-3 letterbox
+run "dialog2-sync.mp4" iris-3 letterbox
+run "dialog3-sync.mp4" iris-3 letterbox
 
-#run "wide/splash3.mp4"   iris-3 default 5110
+run "wide/splash3.mp4"   iris-3 default 5110
 run "wide/attic22.mp4"   iris-3 default 5110
 run "wide/indy4.mp4"     iris-3 default 5110
 run "wide/dialog3-1.mp4" iris-3 default 5110
