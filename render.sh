e8f4ceb598335be2e1a26524b65dba1686354b88
Benjamin Arnaud <bunjee@omega.gg>
2025-06-16 11:46:24 +0200

    room: Update attic (10)

--- render.sh
+++ render.sh
@@ -16,20 +16,15 @@ bin="/c/dev/tools/kdenlive-standalone/bin"
 
 render()
 {
-    local input=$(getPath "$root/dist/room/$1/data/$1.kdenlive")
+    local input=$(getPath "$root/dist/render/room/$1.mlt")
 
-    local output=$(getPath "$root/deploy/$2.mp4")
-
-    local profile=$(getPath "$root/dist/profile/$3.mlt")
+    local output=$(getPath "$root/deploy/$1.mp4")
 
     echo "Rendering from: $input"
     echo "Output:         $output"
 
     # NOTE: These settings are extracted from the kdenlive render panel.
-    ./melt "$input" -profile "$profile" -consumer avformat:"$output" \
-           ab=160k acodec=aac channels=2 crf=23 f=mp4 g=15 movflags=+faststart preset=veryfast \
-           real_time=-1 threads=0 vcodec=libx264 \
-           -progress -verbose
+    ./melt "$input" -consumer avformat:"$output" -progress -verbose
 }
 
 getOs()
@@ -128,12 +123,12 @@ cd "$bin"
 
 if [ $1 = "room/intro" ]; then
 
-    render "intro" "wide/intro" "cinemascope"
+    render "wide/intro" "cinemascope"
 
 elif [ $1 = "room/attic" ]; then
 
-    render "attic"  "wide/attic"  "cinemascope"
-    #render "attic2" "wide/attic2" "cinemascope"
+    render "wide/attic"  "cinemascope"
+    #render "wide/attic2" "cinemascope"
 
 elif [ $1 = "room/attic2" ]; then
 
