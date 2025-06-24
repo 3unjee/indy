dc31b34bba4b99ae5379ae4d75420b4ca1fe58a6
Benjamin Arnaud <bunjee@omega.gg>
2025-06-24 15:54:55 +0200

    room: Update chase (10)

--- dist/room/attic/content/upscale.sh
+++ dist/room/attic/content/upscale.sh
@@ -33,27 +33,25 @@ run()
 
 cd "$ffmpeg"
 
-run "dialog-sync.mp4"  iris-3 letterbox
-run "dialog2-sync.mp4" iris-3 letterbox
-run "dialog3-sync.mp4" iris-3 letterbox
+run "attic25.mp4" iris-3 default 3840 2160 60
 exit 0
 
-run "attic.mp4"     iris-3
-run "attic2.mp4"    iris-3
-run "attic5.mp4"    iris-3
-run "attic6.mp4"    iris-3
-run "attic7.mp4"    iris-3
-run "attic8.mp4"    iris-3
-run "attic11.mp4"   iris-3
-run "attic12.mp4"   iris-3
-run "attic13.mp4"   rhea-1 crop
-run "attic14.mp4"   rhea-1 crop
-run "attic18.mp4"   iris-3
-run "attic19.mp4"   iris-3
-run "attic20.mp4"   iris-3
-run "attic22.mp4"   iris-3
-run "attic24.mp4"   iris-3
-run "attic25.mp4"   iris-3
+run "attic.mp4"   iris-3
+run "attic2.mp4"  iris-3
+run "attic5.mp4"  iris-3
+run "attic6.mp4"  iris-3
+run "attic7.mp4"  iris-3
+run "attic8.mp4"  iris-3
+run "attic11.mp4" iris-3
+run "attic12.mp4" iris-3
+run "attic13.mp4" rhea-1 crop
+run "attic14.mp4" rhea-1 crop
+run "attic18.mp4" iris-3
+run "attic19.mp4" iris-3
+run "attic20.mp4" iris-3
+run "attic22.mp4" iris-3
+run "attic24.mp4" iris-3
+run "attic25.mp4" iris-3
 
 run "indy4.mp4"     iris-3
 run "splash3.mp4"   iris-3
