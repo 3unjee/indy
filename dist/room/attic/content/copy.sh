9689bcb7c59c662b2156b5e87f7a029d13532aab
Benjamin Arnaud <bunjee@omega.gg>
2025-06-30 10:51:09 +0200

    Update movie (13)

--- dist/room/attic/content/copy.sh
+++ dist/room/attic/content/copy.sh
@@ -75,32 +75,30 @@ else
     #----------------------------------------------------------------------------------------------
     # BEGIN copy
 
-    run "attic.mp4"      iris-3 60
-    run "attic2.mp4"     iris-3 60
-    run "attic5.mp4"     iris-3 60
-    run "attic6.mp4"     iris-3 60
-    run "attic7.mp4"     iris-3 60
-    run "attic11.mp4"    iris-3 60
-    run "attic12.mp4"    iris-3 60
-    run "attic18.mp4"    iris-3 60
-    run "attic19.mp4"    iris-3 60
-    run "attic20.mp4"    iris-3 60
-    run "attic22.mp4"    iris-3 24
-    run "attic22-60.mp4" iris-3 60
-    run "attic24.mp4"    iris-3 60
-    run "attic25.mp4"    iris-3 60
+    run "attic.mp4"   iris-3 60
+    run "attic2.mp4"  iris-3 60
+    run "attic5.mp4"  iris-3 60
+    run "attic6.mp4"  iris-3 60
+    run "attic7.mp4"  iris-3 60
+    run "attic11.mp4" iris-3 60
+    run "attic12.mp4" iris-3 60
+    run "attic18.mp4" iris-3 60
+    run "attic19.mp4" iris-3 60
+    run "attic20.mp4" iris-3 60
+    run "attic22.mp4" iris-3 24
+    run "attic24.mp4" iris-3 60
+    run "attic25.mp4" iris-3 60
     run "indy4.mp4"     iris-3 60
     run "splash3.mp4"   iris-3 60
     run "cross6.mp4"    iris-3 60
     run "cross7.mp4"    iris-3 60
     run "gargoyle6.mp4" iris-3 60
-    run "dialog.mp4"     iris-3 60
-    run "dialog2.mp4"    iris-3 60
-    run "dialog3.mp4"    iris-3 24
-    run "dialog3-60.mp4" iris-3 60
-    run "dialog-sync.mp4"  iris-3 60 letterbox
-    run "dialog2-sync.mp4" iris-3 60 letterbox
-    run "dialog3-sync.mp4" iris-3 60 letterbox
+    run "dialog.mp4"  iris-3 30
+    run "dialog2.mp4" iris-3 30
+    run "dialog3.mp4" iris-3 24
+    run "dialog-sync.mp4"  iris-3 30 letterbox
+    run "dialog2-sync.mp4" iris-3 30 letterbox
+    run "dialog3-sync.mp4" iris-3 24 letterbox
     runWide "attic.mp4"     iris-3 60
     runWide "attic2.mp4"    iris-3 60
     runWide "attic5.mp4"    iris-3 60
@@ -118,7 +116,7 @@ else
     runWide "cross6.mp4"    iris-3 60
     runWide "cross7.mp4"    iris-3 60
     runWide "gargoyle6.mp4" iris-3 60
-    runWide "dialog2.mp4"   iris-3 60
+    runWide "dialog2.mp4"   iris-3 30
     runWide "attic11-1.mp4" iris-3 60
     runWide "attic11-2.mp4" iris-3 60
     runWide "dialog3-1.mp4" iris-3 24
