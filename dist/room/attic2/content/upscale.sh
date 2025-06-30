77b296643da50fb2622fa8367a7713998921191c
Benjamin Arnaud <bunjee@omega.gg>
2025-06-30 17:25:47 +0200

    Update attic2

--- dist/room/attic2/content/upscale.sh
+++ dist/room/attic2/content/upscale.sh
@@ -45,20 +45,22 @@ runWide()
 cd "$ffmpeg"
 # END code
 
-run "attic.mp4"  iris-3 60
-run "attic2.mp4" iris-3 60
-run "indy.mp4"   iris-3 60
-run "indy2.mp4"  rhea-1 60 crop
-run "indy4.mp4"  rhea-1 60 crop
-run "indy5.mp4"  iris-3 60
-run "indy6.mp4"  iris-3 60
-run "book.mp4"   iris-3 60
-run "book2.mp4"  iris-3 60
+run "attic.mp4"    iris-3 60
+run "attic2.mp4"   iris-3 60
+run "indy.mp4"     iris-3 60
+run "indy2-v2.mp4" iris-3 60 crop
+run "indy4-v2.mp4" iris-3 60 crop
+run "indy5.mp4"    iris-3 60
+run "indy6.mp4"    iris-3 60
+run "book.mp4"     iris-3 60
+run "book2.mp4"    iris-3 60
 
 runWide "attic.mp4"  iris-3 60
 runWide "attic2.mp4" iris-3 60
 runWide "indy.mp4"   iris-3 60
-runWide "indy2.mp4"  rhea-1 60
-runWide "indy4.mp4"  rhea-1 60
+runWide "indy2.mp4"  iris-3 60
+runWide "indy4.mp4"  iris-3 60
+runWide "indy5.mp4"  iris-3 60
+runWide "indy6.mp4"  iris-3 60
 runWide "book.mp4"   iris-3 60
 runWide "book2.mp4"  iris-3 60
