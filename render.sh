88b69188c95e246d4b8b5bd6e363adfa1015e001
Benjamin Arnaud <bunjee@omega.gg>
2025-07-01 17:34:50 +0200

    Update movie (24)

--- render.sh
+++ render.sh
@@ -199,7 +199,9 @@ cd "$bin"
 
 if [ $1 = "movie" ]; then
 
-    renderBase "movie" "wide"
+    renderBase "movie"            "wide"
+    renderBase "movie" "movie2"   "wide"
+    renderBase "movie" "movieEnd" "wide"
 
 elif [ $1 = "room/intro" ]; then
 
