c4bfc1809b8cbfcb259b2f4fb0043c035f6fe914
bunjee <bunjee@omega.gg>
2025-12-01 12:25:51 +0100

    Update bash

--- render.sh
+++ render.sh
@@ -10,7 +10,7 @@ root="$PWD"
 # NOTE windows: It seems the Program Files path is messing up the melt command line.
 kdenlive="/c/dev/tools/kdenlive/bin"
 
-ffmpeg="$PWD/../Sky-runtime/bash/ffmpeg/bin/ffmpeg"
+ffmpeg="$PWD/../Sky-runtime/bash/video/bin/ffmpeg"
 
 #--------------------------------------------------------------------------------------------------
 # melt
